d0e04269bb4884781220a54f821075b32c37b9bf
bgeun <ayxltme@naver.com>
2022-01-10 16:45:32 +0900

    JS Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -25,7 +25,15 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for samll screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+  console.log("click");
 });
 
 // Handle click on "contact me" button on home
