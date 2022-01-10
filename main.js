9f4c22b0093198b0d608350bfac6238e4819e258
bgeun <ayxltme@naver.com>
2022-01-10 15:49:42 +0900

    JS Navbar

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Make navbar transparent when it is on the top
+
+// html에서 만든 id="navbar"를 가져옴.
+const navbar = document.querySelector("#navbar");
+// navbar의 높이를 가져옴
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+// 스크롤이 될 때마다 등록한 함수를 호출
+// 아무런 인자를 받지 않고 원하는 블록을 실행
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
